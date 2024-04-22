efba4746e3ba804b2d6f01c2669b71c1f562ec42
longhoang <[hoalanh21112002@gmail.com]>
2024-04-22 13:24:15 +0700

    perf: implement prefetch for pagination feature of cabins

--- src/features/cabins/useCabins.ts
+++ src/features/cabins/useCabins.ts
@@ -1,6 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getCabins } from "../../services/cabinApiService";
 import { SortOptions } from "../../interfaces/types";
+import { appConfig } from "../../config";
+
+const { PAGE_LIMIT } = appConfig;
 
 export const useCabins = function ({
   sort = "none",
@@ -9,6 +12,7 @@ export const useCabins = function ({
   sort: SortOptions;
   page: number;
 }) {
+  const queryClient = useQueryClient();
   const options = { sort, page };
   const { data, isLoading, error } = useQuery({
     // queryKey: [`cabins${sort !== "none" ? `-sort-by-${sort}` : ""}`],
@@ -16,6 +20,23 @@ export const useCabins = function ({
     queryFn: () => getCabins(options),
   });
   const { cabins, count } = data || {};
+
+  const numPages = Math.ceil(count / PAGE_LIMIT);
+  if (page > 1 && page <= numPages) {
+    const prefetchOptions = { ...options, page: page - 1 };
+    queryClient.prefetchQuery({
+      queryKey: [`cabins`, prefetchOptions],
+      queryFn: () => getCabins(prefetchOptions),
+    });
+  }
+
+  if (page >= 1 && page < numPages) {
+    const prefetchOptions = { ...options, page: page + 1 };
+    queryClient.prefetchQuery({
+      queryKey: [`cabins`, prefetchOptions],
+      queryFn: () => getCabins(prefetchOptions),
+    });
+  }
 
   return { cabins, isLoading, count, error };
 };
