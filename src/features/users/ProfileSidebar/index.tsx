f7008603a3a2125f410671e6003589d40ab5efd3
longhoang <[hoalanh21112002@gmail.com]>
2024-05-02 16:34:04 +0700

    feat: implement responsive for profile layout

--- src/features/users/ProfileSidebar/index.tsx
+++ src/features/users/ProfileSidebar/index.tsx
@@ -10,15 +10,15 @@ export default function ProfileSidebar() {
 
   if (isLoading) return <Spinner size="normal" />
 
-  return <aside className="w-60 self-start h-[32rem] p-6 bg-stone-100 text-stone-700 rounded-lg ">
-    <div className="flex flex-col justify-center items-center gap-3 mb-8">
+  return <aside className="w-60 self-start h-[32rem] flex flex-col p-6 bg-stone-100 text-stone-700 rounded-lg thin:max-tiny:grid thin:max-tiny:w-full thin:max-tiny:h-full thin:max-tiny:items-center thin:max-tiny:justify-center thin:max-tiny:grid-cols-2 thin:max-tiny:gap-x-12 thin:max-tiny:gap-y-0 ">
+    <div className="flex flex-col justify-center items-center gap-3 mb-8 thin:max-tiny:flex-row  thin:max-tiny:col-span-2 thin:max-tiny:justify-start">
       <img className="rounded-full w-28 h-28" src={avatar} alt="" />
       <div>
         <Heading type="tertiary">{fullName}</Heading>
         <p className="text-xs text-stone-400">Joined in {new Date(createdAt).getFullYear()}</p>
       </div>
     </div>
-    <div className="mb-8">
+    <div className="mb-8 thin:max-tiny:mb-0">
       <p className="text-lg font-semibold">Identity Verification</p>
       <p className="text-xs text-stone-400 leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
     </div>
--- src/layouts/ProfileLayout/index.tsx
+++ src/layouts/ProfileLayout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import ProfileSidebar from "../../features/users/ProfileSidebar";
 
 export default function ProfileLayout() {
-  return <div className="p-12 h-screen grid grid-cols-[1fr_3fr] bg-stone-0">
+  return <div className="p-12 max-h-screen grid grid-cols-[1fr_3fr] bg-stone-0 tiny:max-sm:px-12 gap-x-6 sm:max-md:px-12 thin:max-tiny:grid-cols-1 thin:max-tiny:gap-y-6 thin:max-tiny:px-6">
     <ProfileSidebar />
     <div>
       <Outlet />
