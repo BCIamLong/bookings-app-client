1ef793682ddb3b70de597577e2ff31d8216d8735
longhoang <[hoalanh21112002@gmail.com]>
2024-05-03 09:09:50 +0700

    feat: implement responsive for reset password page

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -4,7 +4,7 @@ export default function ResetPassword() {
   return (
     <div
       style={{ backgroundImage: `url("/imgs/cabins/cabin-007.jpg")` }}
-      className="flex h-screen flex-col items-center gap-6 bg-cover p-12 rounded-l-[20%]"
+      className="flex h-screen flex-col items-center justify-center gap-6 bg-cover p-12 thin:max-tiny:rounded-none rounded-l-[20%]"
     >
       <h2 className="text-3xl font-bold text-stone-100">Reset your password</h2>
       <ResetPasswordForm />
