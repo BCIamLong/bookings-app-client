5dc4d8a3dd4dea5db85992c6bbc3ca3dcd218b56
longhoang <[hoalanh21112002@gmail.com]>
2024-05-03 09:04:57 +0700

    feat: implement responsive for booking success page

--- src/features/bookings/BookingSuccess/index.tsx
+++ src/features/bookings/BookingSuccess/index.tsx
@@ -6,17 +6,17 @@ import Buttons from "../../../components/Buttons";
 import Button from "../../../components/Button";
 
 export default function BookingSuccess() {
-  return <div className="py-12 px-6 bg-brand-100 flex flex-col items-center">
+  return <div className="py-12 px-6 bg-brand-100 flex flex-col items-center thin:max-tiny:px-3 pb-28">
     <div className="flex justify-center items-center gap-2 mb-6">
       <Heading type="secondary">
         <span className="text-brand-600">Payment successfully</span>
         <span><HiOutlineCheckCircle className="text-5xl stroke-[1.5px] text-brand-600" /></span>
       </Heading>
     </div>
-    <div className="flex flex-col items-center justify-center w-[40%]">
+    <div className="flex flex-col items-center justify-center w-[40%] thin:max-tiny:w-[90%] tiny:max-sm:w-[60%] sm:max-md:w-[50%] md:max-xl:w-[48%]">
       <BookingCard />
     </div>
-    <div className="w-[70%] p-6 flex justify-center mt-3 text-sm text-brand-600 items-center">
+    <div className="w-[70%] tiny:max-sm:w-[80%] thin:max-tiny:w-[90%] thin:max-tiny:px-0 gap-3 p-6 flex justify-center mt-3 text-sm text-brand-600 items-center">
       <p>Would you like to receive a notification to remind you when it's time to check in? If you'd like, we can send you a reminder on you start date to ensure you don't miss the beginning of your stay.</p>
       <Buttons>
         <Button type="primary" size="small">Yes</Button>
