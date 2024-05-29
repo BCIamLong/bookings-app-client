db632133642dd4922cea5bf0b577ccaf8de60bfc
longhoang <[hoalanh21112002@gmail.com]>
2024-05-29 12:29:10 +0700

    ci: fix development wrong access enviroment variable

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,17 +1,19 @@
-console.log(process.env.NODE_ENV)
+// console.log(import.meta.env.MODE)
 export default {
   GOOGLE_CLIENt_ID:
     '291632962191-ajj14pb6gqpr9a9q0rmf9c2sqb9bk9kd.apps.googleusercontent.com',
   // GOOGLE_OAUTH_REDIRECT_URL: "http://localhost:3009/api/session/oauth/google",
   GOOGLE_OAUTH_REDIRECT_URL:
-    process.env.NODE_ENV === 'production'
+    import.meta.env.MODE === 'production'
       ? 'https://booking-api-ebe1.onrender.com/api/v1/auth/login/oauth/google'
       : 'http://localhost:3009/api/v1/auth/login/oauth/google',
   SERVER_BASE_URL:
-    process.env.NODE_ENV === 'production'
+    import.meta.env.MODE === 'production'
       ? 'https://booking-api-ebe1.onrender.com'
       : 'http://localhost:3009',
   CLIENT_BASE_UTL:
-    process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5137',
+    import.meta.env.MODE === 'production'
+      ? 'https://bookings-app-client.vercel.app/'
+      : 'http://localhost:5137',
   PAGE_LIMIT: 6,
 }
