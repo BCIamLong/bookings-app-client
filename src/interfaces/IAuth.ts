80eaa081238a8af6dc8a76c7a65ff145147af182
longhoang <[hoalanh21112002@gmail.com]>
2024-05-29 10:47:31 +0700

    perf: improve performance with lazy in react

--- src/interfaces/IAuth.ts
+++ src/interfaces/IAuth.ts
@@ -1,54 +1,54 @@
 export interface UserSession {
-  _id: string;
-  name: string;
-  role: "user" | "admin";
-  enable2FA: boolean;
-  verify2FAOtp?: boolean;
+  _id: string
+  name: string
+  role: 'user' | 'admin'
+  enable2FA: boolean
+  verify2FAOtp?: boolean
 }
 
 export interface SignupInput {
-  name: string;
-  email: string;
-  password: string;
-  passwordConfirm: string;
+  fullName: string
+  email: string
+  password: string
+  passwordConfirm: string
 }
 
 export interface LoginInput {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 
 export interface ForgotPasswordInput {
-  email: string;
+  email: string
 }
 
 export interface ResetPasswordInput {
-  password: string;
-  passwordConfirm: string;
+  password: string
+  passwordConfirm: string
 }
 
 export interface Verify2FAInput {
-  otp: number;
+  otp: number
 }
 
 export interface VerifyEnable2FAInput extends Verify2FAInput {}
 
 export interface EditProfileInput {
-  name: string;
-  avatar: FileList;
+  name: string
+  avatar: FileList
 }
 
 export interface EditEmailInput {
-  email: string;
+  email: string
 }
 
 export interface CheckPasswordInput {
-  password: string;
+  password: string
 }
 
 export interface EditPasswordInput extends ResetPasswordInput {}
 
 export interface DeleteMeInput {
-  reason: string;
-  password: string;
+  reason: string
+  password: string
 }
--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,38 +1,80 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Login from "../pages/Login";
-import SignUp from "../pages/SignUp";
-import ForgotPassword from "../pages/ForgotPassword";
-import ResetPassword from "../pages/ResetPassword";
-import AppLayout from "../layouts/AppLayout";
-import Homepage from "../pages/Homepage";
+import { Suspense, lazy } from "react";
+
+import LoginRoute from "./LoginRoute";
 import ProtectedRoute from "./ProtectedRoute";
-import Verify2FA from "../pages/Verify2FA";
-import PageNotFound from "../pages/PageNotFound";
-import LoginLayout from "../layouts/LoginLayout";
 import Verify2FARoute from "./Verify2FARoute";
-import ProfileLayout from "../layouts/ProfileLayout";
-import UpdateProfileForm from "../features/users/UpdateProfileForm";
-import SettingsLayout from "../layouts/SettingsLayout";
+
+
+import Spinner from "@/components/Spinner";
+
 import Account from "../features/auth/Account";
 import Security from "../features/auth/Security";
 import Enable2FAForm from "../features/auth/Enable2FAForm";
-import Cabins from "../pages/Cabins";
-import Cabin from "../pages/Cabin";
+import UpdateProfileForm from "../features/users/UpdateProfileForm";
 import BookingSuccess from "../features/bookings/BookingSuccess";
 import ReviewsList from "../features/reviews/ReviewsList";
-import ProfileInfoLayout from "../layouts/ProfileInfoLayout";
 import BookingList from "../features/bookings/BookingsList";
-import Bookmarks from "@/pages/Bookmarks";
-import LoginRoute from "./LoginRoute";
-import Contact from "@/pages/Contact";
-import About from "@/pages/About";
+
+// const Account = lazy(() => import("../features/auth/Account"))
+// const Security = lazy(() => import("../features/auth/Security"))
+// const Enable2FAForm = lazy(() => import("../features/auth/Enable2FAForm"))
+// const UpdateProfileForm = lazy(() => import("../features/users/UpdateProfileForm"))
+// const BookingSuccess = lazy(() => import("../features/bookings/BookingSuccess"))
+// const ReviewsList = lazy(() => import("../features/reviews/ReviewsList"))
+// const BookingList = lazy(() => import("../features/bookings/BookingsList"))
+
+
+const AppLayout = lazy(() => import("../layouts/AppLayout"))
+const LoginLayout = lazy(() => import("../layouts/LoginLayout"))
+const ProfileLayout = lazy(() => import("../layouts/ProfileLayout"))
+const SettingsLayout = lazy(() => import("../layouts/SettingsLayout"))
+const ProfileInfoLayout = lazy(() => import("../layouts/ProfileInfoLayout"))
+
+const Login = lazy(() => import("../pages/Login"))
+const SignUp = lazy(() => import("../pages/SignUp"))
+const ForgotPassword = lazy(() => import("../pages/ForgotPassword"))
+const ResetPassword = lazy(() => import("../pages/ResetPassword"))
+const Homepage = lazy(() => import("../pages/Homepage"))
+const Verify2FA = lazy(() => import("../pages/Verify2FA"))
+const PageNotFound = lazy(() => import("../pages/PageNotFound"))
+const Cabins = lazy(() => import("../pages/Cabins"))
+const Cabin = lazy(() => import("../pages/Cabin"))
+const Bookmarks = lazy(() => import("../pages/Bookmarks"))
+const Contact = lazy(() => import("../pages/Contact"))
+const About = lazy(() => import("../pages/About"))
+
+
+
+
+
+
+// import AppLayout from "../layouts/AppLayout";
+// import LoginLayout from "../layouts/LoginLayout";
+// import ProfileLayout from "../layouts/ProfileLayout";
+// import SettingsLayout from "../layouts/SettingsLayout";
+// import ProfileInfoLayout from "../layouts/ProfileInfoLayout";
+
+// import Login from "../pages/Login";
+// import SignUp from "../pages/SignUp";
+// import ForgotPassword from "../pages/ForgotPassword";
+// import ResetPassword from "../pages/ResetPassword";
+// import Homepage from "../pages/Homepage";
+// import Verify2FA from "../pages/Verify2FA";
+// import PageNotFound from "../pages/PageNotFound";
+// import Cabins from "../pages/Cabins";
+// import Cabin from "../pages/Cabin";
+// import Bookmarks from "@/pages/Bookmarks";
+// import Contact from "@/pages/Contact";
+// import About from "@/pages/About";
 // import ProfileInfo from "../features/users/ProfileInfo";
 
 export default function AppRoute() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* <Route element={<AppLayout />}
+      <Suspense fallback={<Spinner size="big" />}>
+        <Routes>
+          {/* <Route element={<AppLayout />}
         >
           <Route path="/profile" element={<ProfileLayout />} >
             <Route index element={<ProfileInfo />} />
@@ -44,55 +86,56 @@ export default function AppRoute() {
             </Route>
           </Route>
         </Route> */}
-        <Route element={
-          <LoginRoute>
-            <LoginLayout />
-          </LoginRoute>
-        }>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route element={
+            <LoginRoute>
+              <LoginLayout />
+            </LoginRoute>
+          }>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password/:token" element={<ResetPassword />} />
 
-        </Route>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Homepage />}></Route>
-          <Route path="/cabins" element={<Cabins />} />
-          <Route path="/cabins/:id" element={<Cabin />} />
-          <Route path="/bookings/success" element={<BookingSuccess />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Route>
-        <Route
-          element={
-            <ProtectedRoute>
-              <AppLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="/login/verify-2fa" element={<Verify2FARoute><Verify2FA /></Verify2FARoute>}></Route>
-          <Route path="/profile" element={<ProfileLayout />} >
-            {/* <Route index element={<ProfileInfo />} /> */}
-            <Route index element={<Navigate replace to='reviews' />} />
-            <Route element={<ProfileInfoLayout />}>
-              <Route path="reviews" element={<ReviewsList isReviewsOfUser={true} />} />
-              <Route path="bookings" element={<BookingList />} />
-            </Route>
-            <Route path="edit" element={<UpdateProfileForm />} />
-            <Route path="setting" element={<SettingsLayout />}>
-              <Route index element={<Navigate replace to='account' />} />
-              <Route path="account" element={<Account />} />
-              <Route path="security" element={<Security />} />
-              <Route path="security/setup-2fa" element={<Enable2FAForm />} />
-            </Route>
           </Route>
-          <Route path="/bookmarks" element={<Bookmarks />} />
-        </Route>
-        {/* <Route element={<LoginLayout />}>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Homepage />}></Route>
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/cabins/:id" element={<Cabin />} />
+            <Route path="/bookings/success" element={<BookingSuccess />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/login/verify-2fa" element={<Verify2FARoute><Verify2FA /></Verify2FARoute>}></Route>
+            <Route path="/profile" element={<ProfileLayout />} >
+              {/* <Route index element={<ProfileInfo />} /> */}
+              <Route index element={<Navigate replace to='reviews' />} />
+              <Route element={<ProfileInfoLayout />}>
+                <Route path="reviews" element={<ReviewsList isReviewsOfUser={true} />} />
+                <Route path="bookings" element={<BookingList />} />
+              </Route>
+              <Route path="edit" element={<UpdateProfileForm />} />
+              <Route path="setting" element={<SettingsLayout />}>
+                <Route index element={<Navigate replace to='account' />} />
+                <Route path="account" element={<Account />} />
+                <Route path="security" element={<Security />} />
+                <Route path="security/setup-2fa" element={<Enable2FAForm />} />
+              </Route>
+            </Route>
+            <Route path="/bookmarks" element={<Bookmarks />} />
+          </Route>
+          {/* <Route element={<LoginLayout />}>
     <Route path="/login" element={<Login />} />
   </Route> */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter >
   );
 }
