407ed4c18eaa6b720f735b6b6459031c4743c910
longhoang <[hoalanh21112002@gmail.com]>
2024-05-29 11:38:25 +0700

    add: setup server origin for production

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,10 +1,17 @@
+console.log(process.env.NODE_ENV)
 export default {
   GOOGLE_CLIENt_ID:
     '291632962191-ajj14pb6gqpr9a9q0rmf9c2sqb9bk9kd.apps.googleusercontent.com',
   // GOOGLE_OAUTH_REDIRECT_URL: "http://localhost:3009/api/session/oauth/google",
   GOOGLE_OAUTH_REDIRECT_URL:
-    'http://localhost:3009/api/v1/auth/login/oauth/google',
-  SERVER_BASE_URL: 'http://localhost:3009',
-  CLIENT_BASE_UTL: 'http://localhost:5137',
+    process.env.NODE_ENV === 'production'
+      ? 'https://booking-api-ebe1.onrender.com/api/v1/auth/login/oauth/google'
+      : 'http://localhost:3009/api/v1/auth/login/oauth/google',
+  SERVER_BASE_URL:
+    process.env.NODE_ENV === 'production'
+      ? 'https://booking-api-ebe1.onrender.com'
+      : 'http://localhost:3009',
+  CLIENT_BASE_UTL:
+    process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5137',
   PAGE_LIMIT: 6,
 }
