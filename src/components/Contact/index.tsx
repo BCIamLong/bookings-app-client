16d436ce21f6f7c3a551540b4845b3b26887999d
longhoang <[hoalanh21112002@gmail.com]>
2024-06-04 10:41:17 +0700

    add: add translation for discover and newsletter components

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,16 +1,18 @@
 import { LuSendHorizonal } from "react-icons/lu";
 import Input from "../form/Input";
+import { useTranslation } from "react-i18next";
 export default function Contact() {
+  const { t } = useTranslation()
   return (
     <div className="flex items-center justify-center gap-12 bg-stone-300 px-3 py-6 text-stone-700">
       <div>
-        <p className="text-xl font-semibold uppercase">NewsLetter</p>
-        <p className="text-xs capitalize text-stone-500">Stay upto date</p>
+        <p className="text-xl font-semibold uppercase">{t('newsletter.heading')}</p>
+        <p className="text-xs capitalize text-stone-500">{t('newsletter.extra-heading')}</p>
       </div>
       <div className="relative flex w-[40%] shadow-lg shadow-stone-300">
         <Input
           type="text"
-          placeholder="Your email"
+          placeholder={t('newsletter.holder')}
           variant="contact"
           id="email"
         />
--- src/components/Discover/index.tsx
+++ src/components/Discover/index.tsx
@@ -1,28 +1,26 @@
+import { useTranslation } from "react-i18next";
 import Button from "../Button";
 import Heading from "../Heading";
 
 export default function Discover() {
+  const { t } = useTranslation()
   return (
     <section className="flex bg-stone-0 items-center gap-24 sm:px-28 tiny:px-12 thin:px-0 py-28 thin:max-tiny:flex-col thin:max-tiny:justify-start" id='discover'>
       <div className="flex w-[70%] flex-col gap-5">
         <div className="mb-2">
           <Heading type="secondary">
-            Discover More About Property Rental
+            {t('discover.heading')}
           </Heading>
           <div className="w-[30%] border-b-4 border-stone-500 p-2 py-3"></div>
         </div>
         <p className="text-sm text-stone-500">
-          At vero eos et accusamus et iusto odio dignissimos ducimus qui
-          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-          et quas molestias excepturi sint occaecati cupiditate non provident,
-          similique sunt in culpa qui officia deserunt mollitia animi, id est
-          laborum et dolorum fuga.
+          {t('discover.description')}
         </p>
         <div className="mb-3 flex gap-4">
-          <Button type="discover-light">Ask A Question</Button>
-          <Button type="discover-light">Find A Property</Button>
+          <Button type="discover-light">{t('discover.links.question')}</Button>
+          <Button type="discover-light">{t('discover.links.find')}</Button>
         </div>
-        <Button type="discover">Discover More</Button>
+        <Button type="discover">{t('discover.btn')}</Button>
       </div>
       <div className="w-[40rem] overflow-hidden rounded-md">
         <img src="/imgs/cabins/cabin-001.jpg" alt="" />
