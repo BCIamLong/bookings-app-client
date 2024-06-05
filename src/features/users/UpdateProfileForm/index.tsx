e2ee4de8dd11663cdcb5c84090b44844cc5acc3a
longhoang <[hoalanh21112002@gmail.com]>
2024-06-05 12:54:52 +0700

    add: add translation for edit profile form(static content)

--- src/features/users/UpdateProfileForm/index.tsx
+++ src/features/users/UpdateProfileForm/index.tsx
@@ -14,8 +14,10 @@ import { useUserSession } from "../../auth/useUserSession";
 import { EditProfileInput } from "../../../interfaces";
 import { useEditProfile } from "../../auth/useEditProfile";
 import { MouseEvent } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function UpdateProfileForm() {
+  const { t } = useTranslation()
   const { editProfile, isEditing } = useEditProfile()
   const { user: { fullName }, isLoading } = useUserSession()
 
@@ -54,15 +56,15 @@ export default function UpdateProfileForm() {
 
   return <Form type="profile" onSubmit={handleSubmit(onSubmit)}>
     <div className="border-b-[1.5px] border-stone-300 py-2 flex justify-between">
-      <Heading type="tertiary">Edit your profile</Heading>
+      <Heading type="tertiary">{t('profile.nav.edit.form.heading')}</Heading>
       <ButtonLink type="secondary" href="/profile" >
         <HiArrowLeft className="text-xl stroke-[1px]" />
-        <span>Back</span>
+        <span>{t('form.btn.back')}</span>
       </ButtonLink>
     </div>
     <FormItem
       type="profile"
-      label="Name"
+      label={t('profile.nav.edit.form.name.label')}
       labelFor="name"
       errorMsg={errors.name?.message}
     >
@@ -70,14 +72,14 @@ export default function UpdateProfileForm() {
         type="text"
         variant="profile"
         id="name"
-        placeholder="Enter your name"
+        placeholder={t('profile.nav.edit.form.name.holder')}
         registerOb={register('name', { required: 'This field is required' })}
 
       />
     </FormItem>
     <FormItem
       type="profile"
-      label="Avatar"
+      label={t('profile.nav.edit.form.avatar.label')}
       labelFor="avatar"
       errorMsg={errors.avatar?.message}
     >
@@ -91,12 +93,12 @@ export default function UpdateProfileForm() {
       />
     </FormItem>
     <div className="flex justify-between items-center w-[80%] py-2 thin:max-lg:w-full">
-      <p className="text-stone-500">All the required user information can be added here...</p>
+      <p className="text-stone-500">{t('profile.nav.edit.form.description')}</p>
       <Buttons>
         <Button type="secondary" size="medium" onClick={resetHandler} >
-          <span></span><span>Cancel</span>
+          <span></span><span>{t('form.btn.cancel')}</span>
         </Button>
-        <Button type="primary" size="medium">{isEditing ? <Spinner size="small" /> : 'Save'}</Button>
+        <Button type="primary" size="medium">{isEditing ? <Spinner size="small" /> : `${t('form.btn.save')}`}</Button>
       </Buttons>
     </div>
 
