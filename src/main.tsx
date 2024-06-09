1350cab7f99c5b96c19c6317980f5525b42281e4
longhoang <[hoalanh21112002@gmail.com]>
2024-06-09 10:27:39 +0700

    feat: implement analytics with vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
+import { Analytics } from "@vercel/analytics/react"
+
 import App from "./App.tsx";
 import "./index.css";
 import ErrorFallback from "./components/ErrorFallback";
@@ -8,6 +10,7 @@ import ErrorFallback from "./components/ErrorFallback";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => window.location.replace("/")}>
+      <Analytics />
       <App />
     </ErrorBoundary>
   </React.StrictMode >,
