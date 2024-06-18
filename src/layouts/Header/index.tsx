5d77ebeea6c08fd4f1698fcf6da3c7e396548a1e
longhoang <longkai21112002@gmail.com>
2024-06-18 10:10:09 +0700

    fix(ui): header is not displayed

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -19,3 +19,12 @@ export default function Header() {
 }
 
 
+
+{/* <motion.header className="h-24 bg-stone-50" initial={{ y: '-30rem', opacity: 1 }} animate={{ y: 0, opacity: 1 }}
+transition={{ type: 'spring', duration: 0.7 }}>
+<div className="flex items-center w-full z-40 fixed justify-between bg-stone-50 px-12 py-3 thin:max-tiny:px-3 tiny:max-sm:px-6">
+  <Logo />
+  <Navigation />
+  <HeaderOptions />
+</div>
+</motion.header > */}
