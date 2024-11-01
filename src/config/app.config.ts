fefd7b7e14ee5b76cff7255aaac4dcd0cc0f6018
longhoang <longkai21112002@gmail.com>
2024-11-01 22:26:34 +0700

    fix: deployment errors

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -10,7 +10,8 @@ export default {
   SERVER_BASE_URL:
     // 'https://booking-api-ebe1.onrender.com',
     import.meta.env.MODE === 'production'
-      ? 'https://booking-api-ebe1.onrender.com'
+      ? // ? 'https://booking-api-ebe1.onrender.com'
+        'https://tours-booking-api.onrender.com/'
       : 'http://localhost:3009',
   CLIENT_BASE_UTL:
     import.meta.env.MODE === 'production'
