5ba0691744370bb84e47d7b8385d000bf71deb25
longhoang <longkai21112002@gmail.com>
2024-11-25 14:30:03 +0700

    update: profile sidebar ui

--- src/features/users/ProfileSidebar/index.tsx
+++ src/features/users/ProfileSidebar/index.tsx
@@ -10,11 +10,13 @@ export default function ProfileSidebar() {
   const { user, isLoading } = useUserSession()
   const { fullName, createdAt, enable2FA, avatar } = user || {}
 
+  const avatarStr = avatar?.includes('default') ? `/${avatar}` : avatar
+
   if (isLoading) return <Spinner size="normal" />
 
   return <aside className="w-full self-start h-[84vh] flex flex-col p-6 bg-stone-100 text-stone-700 rounded-lg thin:max-tiny:grid thin:max-tiny:w-full thin:max-tiny:h-full thin:max-tiny:items-center thin:max-tiny:justify-center thin:max-tiny:grid-cols-2 thin:max-tiny:gap-x-12 thin:max-tiny:gap-y-0 ">
     <div className="flex flex-col justify-center items-center gap-3 mb-8 thin:max-tiny:flex-row  thin:max-tiny:col-span-2 thin:max-tiny:justify-start">
-      <img className="rounded-full w-28 h-28" src={avatar} alt="" />
+      <img className="rounded-full w-28 h-28" src={avatarStr} alt="" />
       <div>
         <Heading type="tertiary">{fullName}</Heading>
         <p className="text-xs text-stone-400">Joined in {new Date(createdAt).getFullYear()}</p>
